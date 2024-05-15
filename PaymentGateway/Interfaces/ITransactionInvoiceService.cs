e98816f5578edcdb5834082868fddea66ee0208f
karim-khaled <karim-khaled@KARIM-KHALED-WI>
2024-05-15 13:04:03 +0300

    bug fix

--- PaymentGateway/Interfaces/ITransactionInvoiceService.cs
+++ PaymentGateway/Interfaces/ITransactionInvoiceService.cs
@@ -4,7 +4,7 @@ namespace PaymentGateway.Interfaces;
 
 public interface ITransactionInvoiceService
 {
-    Task<string> Login(LoginDto loginDto);
+    Task<string> LoginWithHttpClient(LoginDto loginDto);
     Task<string> Pay(GenerateBillDto generateBillDto);
     Task<string> Refund(RefundDto refundDto);
 }
